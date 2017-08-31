070bafe29a385ae118178a01452e1f825eaab355
selva <selvaganapathy@outlook.in>
2017-08-31 06:30:15 +0530

    understood futures :)

--- weather/src/main.rs
+++ weather/src/main.rs
@@ -41,48 +41,15 @@ fn main() {
     let request = client
         .get(url)
         .and_then(|res| res.body().concat2())
-        .map(|body| serde_json::from_slice(&body))
-        .map(|wea| match wea {
-            Ok(Weather { current }) => {
-                println!("{:?}", current);
-                "Weather done"
-            }
-            Err(err) => {
-                println!("Error {:?}", err);
-                "error"
-            }
+        .map_err(|err| std::io::Error::new(std::io::ErrorKind::Other, err))
+        .and_then(|body| {
+            serde_json::from_slice(&body).map_err(|err| {
+                std::io::Error::new(std::io::ErrorKind::Other, err)
+            })
         });
 
-    // request is a Future, futures are lazy, so must explicitly run
-    core.run(request).unwrap();
+    match core.run(request) {
+        Ok(Weather { current }) => println!("{:?}", current),
+        Err(err) => println!("{}", err),
+    }
 }
-
-// fn make_weather_req() -> Future<Item = hyper::Response, Error = std::io::Error> {
-//     let mut core = Core::new()?;
-
-//     let client = Client::new(&core.handle());
-
-//     let url: Uri = "http://api.apixu.com/v1/current.json?key=8e8bca5064f849fe87c102259173008&q=Paris"
-//         .parse()?;
-
-//     let get = client.get("http://httpbin.org/headers".parse()?);
-//     // .and_then(|res| {
-//     // println!("GET: {}", res.status());
-
-//     // res.body().concat2()
-//     // });
-
-//     client.get(url)
-
-
-// }
-
-
-// //.and_then(|res| {
-//     res.body().concat2().and_then(move |body| {
-//         let v: Weather = serde_json::from_slice(&body).unwrap();
-//         Ok(v)
-//     })
-// })
-
-// }
